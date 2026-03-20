ea39538b17fdb43ad1a5b8672ee917f3cc10c06c
talonhowe-dot <talonhowe@gmail.com>
2026-03-20 12:48:13 -0400

    Added animations for Spike
    Includes walking, jumping, and falling, though they all still only face to the right

--- player_1.gd
+++ player_1.gd
@@ -4,6 +4,8 @@ const SPEED = 200
 const JUMP_FORCE = -400
 const GRAVITY = 900
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	# Apply gravity
 	velocity.y += GRAVITY * delta
@@ -14,7 +16,15 @@ func _physics_process(delta):
 
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		_animated_sprite.play("jumpReady")
 		velocity.y = JUMP_FORCE
 
 	# Move the character
 	move_and_slide()
+	
+	if velocity.y > 0 and not is_on_floor():
+		_animated_sprite.play("fall")
+	else: if not velocity.x == 0 and is_on_floor():
+		_animated_sprite.play("walk")
+	else: if is_on_floor():
+		_animated_sprite.play("default")
