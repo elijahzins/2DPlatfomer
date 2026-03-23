337afb9f8e165e0e14141082afb0e689138a58f0
talonhowe-dot <talonhowe@gmail.com>
2026-03-23 15:01:39 -0400

    Updated Spike animations
    Sprite now flips when changing directions
    also he does a little dance if left idle

--- player_1.gd
+++ player_1.gd
@@ -4,7 +4,8 @@ const SPEED = 200
 const JUMP_FORCE = -400
 const GRAVITY = 900
 
-@onready var _animated_sprite = $AnimatedSprite2D
+@onready var playerSprite = $AnimatedSprite2D
+var stopped = 0
 
 func _physics_process(delta):
 	# Apply gravity
@@ -13,18 +14,36 @@ func _physics_process(delta):
 	# Left / Right movement
 	var input_dir = Input.get_axis("ui_left", "ui_right")
 	velocity.x = input_dir * SPEED
+	
+	# Sets sprite direction
+	if input_dir > 0:
+		if playerSprite.flip_h == true:
+			playerSprite.set_flip_h(false)
+	else: if input_dir < 0:
+		if playerSprite.flip_h == false:
+			playerSprite.set_flip_h(true)
+		
 
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		_animated_sprite.play("jumpReady")
+		playerSprite.play("jumpReady")
 		velocity.y = JUMP_FORCE
 
 	# Move the character
 	move_and_slide()
 	
+	# Plays walk cycle or idle
+	
+	if not velocity.x == 0 or (not velocity.y == 0 and not is_on_floor()):
+		stopped = 0
 	if velocity.y > 0 and not is_on_floor():
-		_animated_sprite.play("fall")
+		playerSprite.play("fall")
 	else: if not velocity.x == 0 and is_on_floor():
-		_animated_sprite.play("walk")
+		playerSprite.play("walk")
 	else: if is_on_floor():
-		_animated_sprite.play("default")
+		if stopped > 300:
+			playerSprite.play("idle")
+		else:
+			playerSprite.play("default")
+			stopped += 1
+		
