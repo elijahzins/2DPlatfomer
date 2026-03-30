1e994aa10e387028f986e56f4f7dcc40c76e514c
talonhowe-dot <talonhowe@gmail.com>
2026-03-30 15:08:59 -0400

    Added sprite for enemy
    Generic beetle, has walk cycle and flips based on direction
    Collision has been altered to match

--- enemy.gd
+++ enemy.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 @export var left_limit: float = -100.0
 @export var right_limit: float = 100.0
 
+@onready var enemySprite = $AnimatedSprite2D
+
 var direction := 1
 
 func _ready():
@@ -11,12 +13,18 @@ func _ready():
 	
 func _physics_process(delta):
 	linear_velocity.x = direction * speed
+	enemySprite.play("walk")
 
 	# Flip direction when reaching patrol limits
 	if position.x < left_limit:
 		direction = 1
+		if enemySprite.flip_h == false:
+			enemySprite.set_flip_h(true)
 	elif position.x > right_limit:
 		direction = -1
+		if enemySprite.flip_h == true:
+			enemySprite.set_flip_h(false)
+	
 
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
