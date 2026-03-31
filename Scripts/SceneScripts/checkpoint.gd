9f6cfc53539999e682ae03582b0e7fdc2cb3c166
talonhowe-dot <talonhowe@gmail.com>
2026-03-31 13:17:26 -0400

    Added checkpoint animations and title screen background

--- Scripts/SceneScripts/checkpoint.gd
+++ Scripts/SceneScripts/checkpoint.gd
@@ -1,14 +1,23 @@
 extends Area2D
 
+@onready var checkpointSprite = $AnimatedSprite2D
+var opened = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if opened == false:
+		checkpointSprite.play("closed")
+	else: if checkpointSprite.animation == "opening" and checkpointSprite.frame == 4:
+		checkpointSprite.play("opened")
 
 #if the player's body is a part of the 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("GBLplayer"):
 		GlobalScript.checkpoint_position = $RespawnPoint.global_position
+		if opened == false:
+			opened = true
+			checkpointSprite.play("opening")
