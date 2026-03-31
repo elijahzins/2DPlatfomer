e5aa4c0119304c5681574cf4073d8740d7810be8
Carter Brockman <carteribrockman@gmail.com>
2026-03-31 17:04:14 -0400

    Checkpoint state now carries between scene resets

--- Scripts/SceneScripts/checkpoint.gd
+++ Scripts/SceneScripts/checkpoint.gd
@@ -5,19 +5,21 @@ var opened = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	if GlobalScript.checkpoint_is_open == true:
+		checkpointSprite.play("opened")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if opened == false:
+	if GlobalScript.checkpoint_is_open == false:
 		checkpointSprite.play("closed")
 	else: if checkpointSprite.animation == "opening" and checkpointSprite.frame == 4:
 		checkpointSprite.play("opened")
 
-#if the player's body is a part of the 
+#if the player's body is a part of the "GBLplayer" do the following when entering the checkpoint
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("GBLplayer"):
 		GlobalScript.checkpoint_position = $RespawnPoint.global_position
-		if opened == false:
-			opened = true
+		if GlobalScript.checkpoint_is_open == false:
+			GlobalScript.checkpoint_is_open = true
 			checkpointSprite.play("opening")
+			GlobalScript.checkpoint_is_open = true
--- global_script.gd
+++ global_script.gd
@@ -1,3 +1,5 @@
 extends Node
 
+var checkpoint_is_open = false
+
 var checkpoint_position: Vector2 = Vector2(-999,-999)
