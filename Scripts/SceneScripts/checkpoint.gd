54f72c15607302ea1813b3bc19161109d5b850b6
Carter Brockman <carteribrockman@gmail.com>
2026-03-31 19:48:39 -0400

    Checkpoints will now update when a new checkpoint is reached

--- Scripts/SceneScripts/checkpoint.gd
+++ Scripts/SceneScripts/checkpoint.gd
@@ -5,21 +5,29 @@ var opened = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if GlobalScript.checkpoint_is_open == true:
+	checkpointSprite.play("closed")
+	if GlobalScript.checkpoint_position == $RespawnPoint.global_position:
 		checkpointSprite.play("opened")
+		GlobalScript.previous_checkpoint_node = self
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if GlobalScript.checkpoint_is_open == false:
-		checkpointSprite.play("closed")
-	else: if checkpointSprite.animation == "opening" and checkpointSprite.frame == 4:
+	#if GlobalScript.checkpoint_is_open == false:
+		#checkpointSprite.play("closed")
+	#else: 
+	if checkpointSprite.animation == "opening" and checkpointSprite.frame == 4:
 		checkpointSprite.play("opened")
 
 #if the player's body is a part of the "GBLplayer" do the following when entering the checkpoint
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("GBLplayer"):
+	if body.is_in_group("GBLplayer") && GlobalScript.checkpoint_position != $RespawnPoint.global_position:
 		GlobalScript.checkpoint_position = $RespawnPoint.global_position
 		if GlobalScript.checkpoint_is_open == false:
 			GlobalScript.checkpoint_is_open = true
 			checkpointSprite.play("opening")
-			GlobalScript.checkpoint_is_open = true
+			GlobalScript.checkpoint_is_open = false
+			
+#when encountering a Area2D checkpoint node, close the previous checkpoint and make the new checkpoint the previous
+		if GlobalScript.previous_checkpoint_node:
+			GlobalScript.previous_checkpoint_node.checkpointSprite.play("closed")
+		GlobalScript.previous_checkpoint_node = self
--- global_script.gd
+++ global_script.gd
@@ -3,3 +3,4 @@ extends Node
 var checkpoint_is_open = false
 
 var checkpoint_position: Vector2 = Vector2(-999,-999)
+var previous_checkpoint_node: Area2D = null
