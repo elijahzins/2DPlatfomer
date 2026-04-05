2d6cdaf7fd19706e4821740129e5aa5f045db953
Carter Brockman <carteribrockman@gmail.com>
2026-04-05 14:28:54 -0400

    Added a pause menu that stops the game when activated using 'Esc'
    In each level there will now be a $Pauseable node to signal what nodes must be paused, as some nodes like tilemaps do not need to be paused.
    *Engine.time_scale cannot be used to pause as it creates an issue with player inputs thus get_tree().paused will be used

--- Scripts/LevelScripts/level_1.gd
+++ Scripts/LevelScripts/level_1.gd
@@ -1,20 +1,47 @@
 extends Node2D
 
+@onready var pause_menu_node = $PauseMenu
+var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if GlobalScript.player1Character == "Spike":
 		var player1_scene = preload("res://Sub-Scenes/PlayerCharacterScenes/PlayerSpike.tscn")
 		var player1 = player1_scene.instantiate()
-		add_child(player1)
+		#add_child(player1)
+		get_node("Pauseable").add_child(player1)
 		print("Spike Chosen")
 	
 	if GlobalScript.player1Character == "Schmitty":
 		var player1_scene = preload("res://Sub-Scenes/PlayerCharacterScenes/PlayerSchmitty.tscn")
 		var player1 = player1_scene.instantiate()
-		add_child(player1)
+		#add_child(player1)
+		get_node("Pauseable").add_child(player1)
 		print("Schmitty Chosen")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		pause_menu(paused)
+
+#func pause_menu():
+	#if paused:
+		#pause_menu_node.hide()
+		#Engine.time_scale = 1
+	#else:
+		#pause_menu_node.show()
+		#Engine.time_scale = 0
+	#
+	#paused = !paused
+	
+func pause_menu(is_paused):
+	if is_paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		pause_menu_node.hide()
+		get_tree().paused = false
+
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().paused = true
+		pause_menu_node.show()
+	paused = !paused
--- Scripts/SceneScripts/start_menu.gd
+++ Scripts/SceneScripts/start_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
+	GlobalScript.checkpoint_position = Vector2(-999,-999)
 	get_tree().change_scene_to_file("res://Sub-Scenes/UIScenes/character_select.tscn")
 
 func _on_quit_button_pressed():
